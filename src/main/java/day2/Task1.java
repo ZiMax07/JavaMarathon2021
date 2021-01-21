c92e1beb9a5f5c5c8a6ee58e150c3d64d8fd86c9
Zherenkov Maxim <for_wellaje_@mail.ru>
2021-01-21 13:53:05 +0700

    Add Day2 solution

--- src/main/java/day2/Task1.java
+++ src/main/java/day2/Task1.java
@@ -1,7 +1,20 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+    Scanner console = new Scanner(System.in);
+    int floors = console.nextInt();
+    if (floors <= 4 && floors > 0){
+        System.out.println("Малоэтажный дом");
+    } else if(floors >= 5 && floors < 9){
+        System.out.println("Среднеэтажный дом");
+    } else if (floors >= 9) {
+        System.out.println("Многоэтажный дом");
+    } else {
+        System.out.println("Ошибка ввода");
+    }
 
     }
 }
--- src/main/java/day2/Task3.java
+++ src/main/java/day2/Task3.java
@@ -1,7 +1,21 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
-
+        Scanner console = new Scanner(System.in);
+        int a = console.nextInt();
+        int b = console.nextInt();
+        if (a >= b) {
+            System.out.println("Некорректный ввод");
+        }
+        int c = a + 1;
+        while (c < b) {
+            if (c % 5 == 0 && c % 10 != 0) {
+                System.out.println(c);
+                c++;
+            }
+        }
     }
 }
