aeca00afb1e8a5c98ef89df78db3c4530132f822
Zherenkov Maxim <for_wellaje_@mail.ru>
2021-02-05 15:25:08 +0700

    Add Day16 solution

--- src/main/java/day16/Task1.java
+++ src/main/java/day16/Task1.java
@@ -1,8 +1,38 @@
 package day16;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
 
+        String separator = File.separator;
+        File file = new File(separator + "C:" + separator + "Users" + separator + "wella" + separator + "Desktop"
+                + separator + "Marathon" + separator + "JavaMarathon2021" + separator +
+                "src" + separator + "main" + separator + "java" + separator + "day16" + separator + "numbers");
+
+        printResult(file);
+
+
+    }
+        public static void printResult(File file) {
+            try {
+                Scanner scanner = new Scanner(file);
+
+                int sum = 0;
+                int count = 0;
+                while (scanner.hasNextLine()){
+                    sum += scanner.nextInt();
+                    count++;
+                }
+                double average = (double) sum / count;
+                String formattedDouble = new DecimalFormat("#0.000").format(average);
+                System.out.println(average + " --> " + formattedDouble);
+                scanner.close();
+            } catch (FileNotFoundException e) {
+                System.out.println("Файл не найден");
+            }
     }
 }
-
--- src/main/java/day16/Task2.java
+++ src/main/java/day16/Task2.java
@@ -1,7 +1,71 @@
 package day16;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
 
+        String separator = File.separator;
+        File file1 = new File(separator + "C:" + separator + "Users" + separator + "wella" + separator + "Desktop"
+                + separator + "Marathon" + separator + "JavaMarathon2021" + separator +
+                "src" + separator + "main" + separator + "java" + separator + "day16" + separator + "file1.txt");
+
+        File file2= new File(separator + "C:" + separator + "Users" + separator + "wella" + separator + "Desktop"
+                + separator + "Marathon" + separator + "JavaMarathon2021" + separator +
+                "src" + separator + "main" + separator + "java" + separator + "day16" + separator + "file2.txt");
+
+        try {
+            PrintWriter pw = new PrintWriter(file1);
+            for (int i = 0; i < 1000; i++){
+                int random = (int) (Math.random()*100) + 1;
+                pw.println(random);
+            }
+            pw.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        }
+        try {
+            PrintWriter pw2 = new PrintWriter(file2);
+            Scanner scanner = new Scanner(file1);
+            List<Integer> numbers = new ArrayList<>();
+            int sum = 0;
+            double average;
+            while (scanner.hasNextLine()){
+                String line = scanner.nextLine();
+                numbers.add(Integer.parseInt(line));
+            }
+            for (int i = 0; i < numbers.size(); i +=20){
+                for (int j = i; j < i + 20;j++){
+                    sum += numbers.get(j);
+                }
+                average = sum / 20.0;
+                pw2.println(average);
+                sum = 0;
+            }
+            pw2.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        }
+
+        printResult(file2);
+    }
+
+    public static void printResult(File file) {
+        try {
+            Scanner scanner = new Scanner(file);
+            double sum = 0;
+            while (scanner.hasNextLine()){
+                String line = scanner.nextLine();
+                sum += Float.parseFloat(line);
+            }System.out.println(sum);
+            System.out.println((int) sum);
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        }
     }
 }
